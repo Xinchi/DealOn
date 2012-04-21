b0891a8f539f25205f3a83da8664dc1c3be14fc0
Gu Xinchi <guxinchi2000@gmail.com>
2012-04-21 21:18:46 +0800

    Created DealOn

---
+++ DealOn/DealOn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DealOn
+//
+//  Created by Max Gu on 4/21/12.
+//  Copyright (c) 2012 guxinchi2000@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DealOn/DealOnTests/DealOnTests.m
@@ -0,0 +1,32 @@
+//
+//  DealOnTests.m
+//  DealOnTests
+//
+//  Created by Max Gu on 4/21/12.
+//  Copyright (c) 2012 guxinchi2000@gmail.com. All rights reserved.
+//
+
+#import "DealOnTests.h"
+
+@implementation DealOnTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DealOnTests");
+}
+
+@end
