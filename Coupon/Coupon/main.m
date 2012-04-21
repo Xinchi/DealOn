869fb385f87944d4209ad3c94c7f35c2bd7cb193
Gu Xinchi <guxinchi2000@gmail.com>
2012-04-21 21:06:13 +0800

    Coupon Created

---
+++ Coupon/Coupon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Coupon
+//
+//  Created by Max Gu on 4/21/12.
+//  Copyright (c) 2012 guxinchi2000@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Coupon/CouponTests/CouponTests.m
@@ -0,0 +1,32 @@
+//
+//  CouponTests.m
+//  CouponTests
+//
+//  Created by Max Gu on 4/21/12.
+//  Copyright (c) 2012 guxinchi2000@gmail.com. All rights reserved.
+//
+
+#import "CouponTests.h"
+
+@implementation CouponTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CouponTests");
+}
+
+@end
